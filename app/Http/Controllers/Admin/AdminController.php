b3251a41c5e74ed27ac57283c322011036c66245
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2022-12-13 15:35:06 +0700

    fix(shob): handle error

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -24,23 +24,23 @@ class AdminController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            "name"          => "required|min:5",
+            "email"         => "required|email",
+            "password_text" => "required|min:5",
+        ]);
+
+        $request->merge([
+            "password" => Hash::make($request->password_text),
+            "role"     => User::ROLE_ADMIN,
+        ]);
+
         try {
-            $request->validate([
-                "name"          => "required|min:5",
-                "email"         => "required|email",
-                "password_text" => "required|min:5",
-            ]);
-    
-            $request->merge([
-                "password" => Hash::make($request->password_text),
-                "role"     => User::ROLE_ADMIN,
-            ]);
-    
-            User::create($request->except('_token', 'password_text'));
-            
+            User::create($request->except('_token', 'password_text')); 
             return redirect()->route('admin.index')->with('success', 'Data admin berhasil ditambahkan!');
         } catch(\Exception $e) {
-            return redirect()->route('admin.index')->with('error_msg', 'Data admin gagal ditambahkan!');
+            logger()->error("error creating admin: ".$e->getMessage());
+            return redirect()->back()->with('error_msg', 'Data Admin gagal ditambahkan!');
         }
     }
 
@@ -53,17 +53,17 @@ class AdminController extends Controller
 
     public function update(Request $request, $id)
     {
+        $request->validate([
+            "name"          => "required|min:5",
+            "email"         => "required|email",
+        ]);
+
         try {
-            $request->validate([
-                "name"          => "required|min:5",
-                "email"         => "required|email",
-            ]);
-    
             User::where('id', $id)->update($request->except('_token', '_method'));
-    
             return redirect()->route('admin.index')->with('success', 'Data admin berhasil diubah!');
         } catch(\Exception $e) {
-            return redirect()->route('admin.index')->with('error_msg', 'Data admin gagal diubah!');
+            logger()->error("error update blog: ".$e->getMessage());
+            return redirect()->back()->with('error_msg', 'Data Admin gagal diubah!');
         }
     }
 
@@ -76,18 +76,18 @@ class AdminController extends Controller
 
     public function updatePassword(Request $request, $id)
     {
+        $request->validate([
+            "password"          => "required|min:5|confirmed",
+        ]);
+
         try {
-            $request->validate([
-                "password"          => "required|min:5|confirmed",
-            ]);
-    
             User::where('id', $id)->update([
                     "password" => Hash::make("password")
             ]);
-    
             return redirect()->route('admin.index')->with('success', 'Password berhasil diubah!');
         } catch(\Exception $e) {
-            return redirect()->route('admin.index')->with('error_msg', 'Password gagal diubah!');
+            logger()->error("error update passsword admin: ".$e->getMessage());
+            return redirect()->back()->with('error_msg', 'Password gagal diubah!');
         }
     }
 }
